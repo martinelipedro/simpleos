2fc904ee1594f5bf53100883f2cb084799170873
martinelipedro <martinelipedro@gmail.com>
2023-11-05 11:19:33 -0300

    feat: add function for remapping pic

--- src/idt.c
+++ src/idt.c
@@ -1,10 +1,13 @@
 #include "idt.h"
 
+#include "util.h"
+
 idt_entry_T idt_entries[256];
 idt_pointer_T idt_pointer;
 
 extern void idt_flush(void);
 static void idt_set_entry(uint8_t index, uint32_t isr, uint8_t flags);
+static void remap_pic(void);
 
 void init_idt(void)
 {
@@ -44,7 +47,23 @@ void init_idt(void)
     idt_set_entry(30, (uint32_t)isr30, 0x8E);
     idt_set_entry(31, (uint32_t)isr31, 0x8E);
 
+    remap_pic();
+
     idt_flush();
+}
+
+static void remap_pic(void)
+{
+    outb(0x20, 0x11);
+    outb(0xA0, 0x11);
+    outb(0x21, 0x20);
+    outb(0xA1, 0x28);
+    outb(0x21, 0x04);
+    outb(0xA1, 0x02);
+    outb(0x21, 0x01);
+    outb(0xA1, 0x01);
+    outb(0x21, 0x0);
+    outb(0xA1, 0x0);
 }
 
 static void idt_set_entry(uint8_t index, uint32_t isr, uint8_t flags)
